13fd631a1f33c1bee202877e1ddbe7b7bd988fb3
nike951847 <sos7mi@gmail.com>
2020-06-06 11:29:45 +0800

    ClassC

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/emu/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/emu/YourService.java
@@ -1,6 +1,7 @@
 package jp.jaxa.iss.kibo.rpc.emu;
 
 import android.graphics.Bitmap;
+import android.text.LoginFilter;
 import android.util.Log;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 
@@ -15,7 +16,12 @@ import com.google.zxing.*;
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.common.HybridBinarizer;
 import java.lang.String;
+import java.lang.Math;
+
+
 public class YourService extends KiboRpcService {
+    static int finishQR = 0;
+    static double xpoz,ypoz,zpoz,xqua,yqua,zqua,wqua;
     @Override
     protected void runPlan1() {
         api.judgeSendStart();
@@ -23,38 +29,110 @@ public class YourService extends KiboRpcService {
         moveToWrapper(11.5, -5.7, 4.5, 0, 0, 0, 1);
         Log.d("Timer","____moveToWrapper(11.5, -5.7, 4.5, 0, 0, 0, 1);finish");
         waiting();
+
         Log.d("Timer","____getQRstart");
-        getQR(0);
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+             getQR(0);
+             finishQR ++;
+             // your code here ...
+            }
+        });
+
+        t.start();
+       // getQR(0);
         Log.d("Timer","____getQRfinish");
         waiting();
         Log.d("Timer","____");
-        moveToWrapper(11,-6, 5.55, 0, -0.7071068, 0, 0.7071068);
+        moveToWrapper(11,-6, 5.52, 0, -0.7071068, 0, 0.7071068);
         Log.d("Timer","____");
-        getQR(1);
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                getQR(1);
+                finishQR ++;// your code here ...
+            }
+        });
+
+        t1.start();
         Log.d("Timer","____");
         waiting();
         Log.d("Timer","____");
-        moveToWrapper(11,-5.5, 4.33, 0, -0.7071068, 0, 0.7071068);
+        moveToWrapper(11,-5.5, 4.35, 0, 0.7071068, 0, 0.7071068);
         Log.d("Timer","____");
-        getQRD(2);
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                getQR(2);   // your code here ...
+                finishQR ++;
+            }
+        });
+
+        t2.start();
+        moveToWrapper(10.5,-5.5, 4.5, 0, 0.7071068, 0, 0.7071068);
+        moveToWrapper(10.3,-7, 4.6, 0, 0.7071068, 0, 0.7071068);
+        moveToWrapper(11.3,-7, 4.6, 0, 0.7071068, 0, 0.7071068);
+        moveToWrapper(11.3,-7.5, 4.6, 0, 0.7071068, 0, 0.7071068);
         moveToWrapper(10.30,-7.5, 4.7,0, 0, 1, 0);
+
         Log.d("Timer","____");
         waiting();
-        getQR(3);
+        Thread t3 = new Thread(new Runnable() {
+            public void run() {
+                getQR(3);   // your code here ...
+                finishQR ++;
+
+                }
+        });
+
+        t3.start();
         Log.d("Timer","____");
+
         moveToWrapper(11.5, -8, 5,0, 0, 0, 1);
         Log.d("Timer","____");
         waiting();
         getQR(4);
-        moveToWrapper(11, -7.7, 5.55 , 0, -0.7071068, 0, 0.7071068);
-        waiting();
-        getQR(5);
-        moveToWrapper(11, -7.7, 5.55 , 0, -0.7071068, 0, 0.7071068);
-        api.judgeSendFinishSimulation();
-      /*
+        Thread t4 = new Thread(new Runnable() {
+            public void run() {
+                getQR(4);   // your code here ...
+                finishQR++;
+            }
+        });
 
-        String[] ypoz =xposition.split(",");
-        double ypos = Double.parseDouble(xpoz[1]);
+        t4.start();
+        moveToWrapper(11, -7.7, 5.4 , 0, -0.7071068, 0, 0.7071068);
+        waiting();
+        Thread t5 = new Thread(new Runnable() {
+            public void run() {
+                getQR(5);   // your code here ...
+                finishQR++;
+            }
+        });
+
+        t5.start();
+
+
+
+
+
+        while (finishQR<6){
+        }
+        Log.d("finalqr:","x"+xpoz+"y"+ypoz+"z"+zpoz+"x"+xqua+"y"+yqua+"z"+zqua+"w"+getquaw());
+        moveToWrapper(xpoz, ypoz, zpoz, xqua, yqua, zqua, getquaw());
+        Log.d("moveto", "finalQR");
+
+
+        Thread t6 = new Thread(new Runnable() {
+            public void run() {
+                getQR(6);   // your code here ...
+                finishQR++;
+            }
+        });
+
+        t6.start();
+
+        api.judgeSendFinishSimulation();
+        /*
+
+
 */
 
 
@@ -76,6 +154,7 @@ public class YourService extends KiboRpcService {
         final Point point = new Point(pos_x, pos_y, pos_z);
         final Quaternion quaternion = new Quaternion((float) qua_x, (float) qua_y,
                 (float) qua_z, (float) qua_w);
+
 
         Result result = api.moveTo(point, quaternion, true);
 
@@ -136,13 +215,14 @@ public class YourService extends KiboRpcService {
             Log.d(tag, "nope");
             getQRString = convert(api.getBitmapNavCam());
             if (getQRString != null) {
-
+                sort(number, getQRString);
                 api.judgeSendDiscoveredQR(number, getQRString);
             }
 
         } else if (getQRString != null) {
             try {
                 Log.d(tag, getQRString);
+                sort(number, getQRString);
                 api.judgeSendDiscoveredQR(number, getQRString);
             } catch (Exception e) {
                 Log.e("tagerror", "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
@@ -159,13 +239,14 @@ public class YourService extends KiboRpcService {
             Log.d(tag, "nope");
             getQRString = convert(api.getBitmapDockCam());
             if (getQRString != null) {
-
+                sort(number, getQRString);
                 api.judgeSendDiscoveredQR(number, getQRString);
             }
 
         } else if (getQRString != null) {
             try {
                 Log.d(tag, getQRString);
+                sort(number, getQRString);
                 api.judgeSendDiscoveredQR(number, getQRString);
             } catch (Exception e) {
                 Log.e("tagerror", "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
@@ -173,6 +254,44 @@ public class YourService extends KiboRpcService {
         }
         return getQRString;
     }
+    private double getquaw(){
+        wqua = Math.pow(1-xqua*xqua-yqua*yqua-zqua*zqua,0.5);
+        return wqua;
+    }
+    private void sort(int number,String qrcode){
+
+        String[] split =qrcode.split(",");
+        Log.d("qrcodesort", split[0]);
+        Log.d("qrcodesort", split[1]);
+        double sp = Double.parseDouble(split[1]);
+        if(number==0){
+            xpoz=sp;
+            Log.d("qrcodesort", split[1]);
+        }
+        else if (number==1){
+            ypoz=sp;
+            Log.d("qrcodesort", split[1]);
+        }
+        else if (number==2){
+            zpoz=sp;
+            Log.d("qrcodesort", split[1]);
+        }
+        else if (number==3){
+            xqua=sp;
+            Log.d("qrcodesort", split[1]);
+        }
+        else if (number==4){
+            yqua=sp;
+            Log.d("qrcodesort", split[1]);
+        }
+        else if (number==5){
+            zqua=sp;
+            Log.d("qrcodesort", split[1]);
+        }
+
+
+    }
+
 
 }
 
