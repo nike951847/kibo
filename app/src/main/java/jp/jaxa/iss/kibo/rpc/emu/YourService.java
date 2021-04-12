ed97e9296236e65fefc6842da48a2f0946b9955c
nike951847 <47024451+nike951847@users.noreply.github.com>
2021-04-12 14:58:10 +0800

    修改路徑

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/emu/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/emu/YourService.java
@@ -1,4 +1,4 @@
 連結重新導向
-FRC程式連結為  "https://github.com/FRC7636Robomania"
+FRC程式連結為  "https://github.com/FRC7636Robomania/2020main"
 KiboPRC程式連結為  "https://github.com/nike951847/KiboPRC/tree/master/app/src/main/java/jp/jaxa/iss/kibo/rpc/emu"
 //抱歉造成您的困擾
