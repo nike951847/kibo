71f0de3b2d146370c70ee872876f64b46a1573bc
nike951847 <47024451+nike951847@users.noreply.github.com>
2021-04-12 14:08:22 +0800

    重新導向

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/emu/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/emu/YourService.java
@@ -1,3 +1,4 @@
 連結重新導向
-FRC程式連結為  "https://github.com/FRC7636Robomania 抱歉造成您的困擾。"
+FRC程式連結為  "https://github.com/FRC7636Robomania"
 KiboPRC程式連結為  "https://github.com/nike951847/KiboPRC/tree/master/app/src/main/java/jp/jaxa/iss/kibo/rpc/emu"
+//抱歉造成您的困擾
